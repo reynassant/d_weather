0505433d8864c0520bf983b4d00f598af0462b32
Juampa <juan.pa.pe.co.98@gmail.com>
2022-09-08 13:10:18 +0200

    Fetch para get API (madrid)

---
+++ tiempos.js
@@ -0,0 +1,9 @@
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=madrid&APPID=ccab882185f6b3e0e5bf39ec3eb0d330&units=metric", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error)); 
