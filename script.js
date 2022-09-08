387386bbfa66c57d6d323e27ce886b2562db0fd2
Juampa <juan.pa.pe.co.98@gmail.com>
2022-09-08 15:17:21 +0200

    actualizacion muy breve

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const mTiempo = document.getElementById("time");
+
+
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
   
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=madrid&APPID=ccab882185f6b3e0e5bf39ec3eb0d330&units=metric", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error)); 
+fetch("https://api.openweathermap.org/data/2.5/weather?q=madrid&APPID=ccab882185f6b3e0e5bf39ec3eb0d330&units=metric", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error)); 
+
