436bdd62b0ca9fb96e575cda40d0b02e78abf8ed
Alfonso Perez <alfonspm10@gmail.com>
2022-09-09 12:37:12 +0200

    prototipo final, script terminado y vista responsive

--- script.js
+++ script.js
@@ -31,16 +31,73 @@ setInterval(() => {
 const API_KEY = '49cc8c821cd2aff9af04c9f98c36eb74'
 
 getWeather()
-
 function getWeather(){
-    navigator.geolocation.getCurrentPosition((success) => {
-        let {latitude, longitude } = success.coords;
-
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
-
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log('error', error)); 
+        navigator.geolocation.getCurrentPosition((success) => {
+            
+            let {latitude, longitude } = success.coords;
+    
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
+            .then(res => res.json())
+            .then(data => {
+    
+            console.log(data)
+            showWeatherData(data);
+            })
+    
         })
+}
 
-    };
+
+function showWeatherData (data){
+    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+
+    fTimeZone.innerHTML = data.timezone;
+
+    fCWeathITEMS.innerHTML = 
+    `<div class="weather-item">
+        <div>Humedad</div>
+        <div>${humidity}%</div>
+    </div>
+    <div class="weather-item">
+        <div>Presión</div>
+        <div>${pressure}</div>
+    </div>
+    <div class="weather-item">
+        <div>Velocidad del Viento</div>
+        <div>${wind_speed}</div>
+    </div>
+    <div class="weather-item">
+        <div>Salida del Sol</div>
+        <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
+    </div>
+    <div class="weather-item">
+        <div>Puesta del Sol</div>
+        <div>${window.moment(sunset*1000).format('HH:mm a')}</div>
+    </div>
+    
+    `;
+    let otherDays = ''
+    data.daily.forEach((day, idx) =>{
+        if (idx == 0){
+            fCurrentTemp.innerHTML= `
+            <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+            <div class="otro">
+                <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+                <div class="temp">Noche - ${day.temp.night}&#176;C</div>
+                <div class="temp">Dia - ${day.temp.day}&#176;C</div>
+            </div>
+            `
+        }else{
+            otherDays += `
+            <div class="weather-forecast-item">
+                <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                <div class="temp">Noche - ${day.temp.night}&#176;C</div>
+                <div class="temp">Dia - ${day.temp.day}&#176;C</div>
+            </div>
+            `
+        }
+
+    })
+    fWeathForec.innerHTML = otherDays;
+}
